c007e95a2c1aed2e9cd2792c03e84f0b856e13f3
DeveloperRyou <DeveloperRyou@gmail.com>
2022-04-13 17:41:58 +0900

    fix shell

--- tests/test.sh
+++ tests/test.sh
@@ -1,23 +1,26 @@
-# test many size
+# test many size (50)
 for file in $(ls ./tests/ | grep test_fix_N_)
 do
+	echo "Test "$file
 	echo $file | tr '\n' '\t' >> ./tests/test_fix_N_output
 	./program1 avg < ./tests/$file | head -1 | tr '\n' '\t'  >> ./tests/test_fix_N_output
 	./program2 avg 4 < ./tests/$file | head -1 | tr '\n' '\t' >> ./tests/test_fix_N_output
 	./program3 avg 4 < ./tests/$file | head -1 >> ./tests/test_fix_N_output
 done
-# test many N
+# test many N (30)
 for file in $(ls ./tests/ | grep test_fix_size_)
 do
+	echo "Test "$file
 	echo $file | tr '\n' '\t' >> ./tests/test_fix_size_output
 	./program1 avg < ./tests/$file | head -1 | tr '\n' '\t'  >> ./tests/test_fix_size_output
 	./program2 avg 4 < ./tests/$file | head -1 | tr '\n' '\t' >> ./tests/test_fix_size_output
 	./program3 avg 4 < ./tests/$file | head -1 >> ./tests/test_fix_size_output
 done
-# test many process
-for process in {1..60..1}
+# test many process (50)
+for process in {1..50..1}
 do
 	file=test_fix_Nsize
+	echo "Test "$file"_"$process
 	echo $file"_"$process | tr '\n' '\t' >> ./tests/test_fix_Nsize_output
 	./program1 avg < ./tests/$file | head -1 | tr '\n' '\t'  >> ./tests/test_fix_Nsize_output
 	./program2 avg $process < ./tests/$file | head -1 | tr '\n' '\t' >> ./tests/test_fix_Nsize_output
--- tests/testfiles.sh
+++ tests/testfiles.sh
@@ -1,20 +1,20 @@
-# 60 files fix N
-for var in {1000..60000..1000}
+# 50 files fix N
+for var in {500..25000..500}
 do
 	file=./tests/test_fix_N_$(printf "%.5d" $var)
 	./tests/test $var $var 10 > $file
 	echo "Make "$file
 done
-# 60 files fix size
-for var in {1..60000..1}
+# 30 files fix size
+for var in {1..20000..1}
 do
-	if [ $((60000 % $var)) -eq 0 ]; then
+	if [ $((20000 % $var)) -eq 0 ]; then
 		file=./tests/test_fix_size_$(printf "%.5d" $var)
-		./tests/test 60000 60000 $var > $file
+		./tests/test 20000 20000 $var > $file
 		echo "Make "$file
 	fi
 done
 # fix N and fix size 
-./tests/test 60000 60000 10 > ./tests/test_fix_Nsize
+./tests/test 25000 25000 10 > ./tests/test_fix_Nsize
 echo "Make ./tests/test_fix_Nsize"
 echo "----Finish to Make Test Files----"
