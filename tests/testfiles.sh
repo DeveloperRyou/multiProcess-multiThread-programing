96b1da6b508cdd6661c1746be0a9ec2566f61501
DeveloperRyou <DeveloperRyou@gmail.com>
2022-04-13 23:05:53 +0900

    fix testsh

--- tests/testfiles.sh
+++ tests/testfiles.sh
@@ -1,20 +1,20 @@
-# 25 files fix N
-for var in {400..10000..400}
+# 20 files fix N
+for var in {250..5000..250}
 do
-	file=./tests/test_fix_N_$(printf "%.5d" $var)
+	file=./tests/test_fix_N_$(printf "%.4d" $var)
 	./tests/test $var $var 10 > $file
 	echo "Make "$file
 done
-# 25 files fix size
-for var in {1..10000..1}
+# 20 files fix size
+for var in {1..5000..1}
 do
-	if [ $((10000 % $var)) -eq 0 ]; then
-		file=./tests/test_fix_size_$(printf "%.5d" $var)
-		./tests/test 10000 10000 $var > $file
+	if [ $((5000 % $var)) -eq 0 ]; then
+		file=./tests/test_fix_size_$(printf "%.4d" $var)
+		./tests/test 5000 5000 $var > $file
 		echo "Make "$file
 	fi
 done
 # fix N and fix size 
-./tests/test 10000 10000 10 > ./tests/test_fix_Nsize
+./tests/test 5000 5000 10 > ./tests/test_fix_Nsize
 echo "Make ./tests/test_fix_Nsize"
 echo "----Finish to Make Test Files----"
