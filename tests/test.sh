e19e5e7e58534995064480c82bc96fcf324d2a1b
DeveloperRyou <DeveloperRyou@gmail.com>
2022-04-18 01:54:51 +0900

    testsh

---
+++ tests/test.sh
@@ -0,0 +1,28 @@
+# test many size (10)
+for file in $(ls ./tests/ | grep test_fix_N_)
+do
+	echo "Test "$file
+	echo $file | tr '\n' '\t' >> ./tests/test_fix_N_output
+	./program1 avg < ./tests/$file | head -1 | tr '\n' '\t'  >> ./tests/test_fix_N_output
+	./program2 avg 4 < ./tests/$file | head -1 | tr '\n' '\t' >> ./tests/test_fix_N_output
+	./program3 avg 4 < ./tests/$file | head -1 >> ./tests/test_fix_N_output
+done
+# test many N (12)
+for file in $(ls ./tests/ | grep test_fix_size_)
+do
+	echo "Test "$file
+	echo $file | tr '\n' '\t' >> ./tests/test_fix_size_output
+	./program1 avg < ./tests/$file | head -1 | tr '\n' '\t'  >> ./tests/test_fix_size_output
+	./program2 avg 4 < ./tests/$file | head -1 | tr '\n' '\t' >> ./tests/test_fix_size_output
+	./program3 avg 4 < ./tests/$file | head -1 >> ./tests/test_fix_size_output
+done
+# test many process (30)
+for process in {1..30..1}
+do
+	file=test_fix_Nsize
+	echo "Test "$file"_"$process
+	echo $file"_"$process | tr '\n' '\t' >> ./tests/test_fix_Nsize_output
+	./program1 avg < ./tests/$file | head -1 | tr '\n' '\t'  >> ./tests/test_fix_Nsize_output
+	./program2 avg $process < ./tests/$file | head -1 | tr '\n' '\t' >> ./tests/test_fix_Nsize_output
+	./program3 avg $process < ./tests/$file | head -1 >> ./tests/test_fix_Nsize_output
+done
